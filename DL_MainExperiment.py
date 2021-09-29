b77cedf658e6a0bce9d2e206714f78ba951620ce
joespeak <62236886+joespeak@users.noreply.github.com>
2021-09-29 14:43:08 +0800

    Update DL_MainExperiment.py

--- DL_MainExperiment.py
+++ DL_MainExperiment.py
@@ -3,3 +3,16 @@ from utils.random5Foldstrain import *
 seed_torch(2021)
 model = "mobileLarge"
 manyTimesTrain(model, config.manyTimesTrainFirstPath, times=5, modelName="mobileBigBig")
+
+'''    seed_torch(2021)
+    model = "mobileLarge"
+
+    #before begin training plz save your data as data
+    #data format should be "data/class1" "data/class2" and so on
+    makeCrossValidation = MakeCrossValidationDataFolder(config.crossValidationFolds, 50)
+    cvFirstPath = makeCrossValidation.begin_split()
+    #Attention !!!!!!!
+    #The relative path is relative to the currently running py file,so we must change r"../crossValidationData/...." to r"./crossValidationData/...."
+    cvFirstPath = cvFirstPath[1:]
+    manyTimesTrain(model, cvFirstPath, times=config.crossValidationFolds, modelName="mobileBigBig")
+'''
